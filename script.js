c7bcb7b9b1eabbad61cbb13bf08af641c01bf86e
p1521303x <miteshmohan08@gmail.com>
2017-11-27 18:20:10 +0000

    finished homepage, added images and social media icons also created javascript file

---
+++ script.js
@@ -0,0 +1,15 @@
+<script 
+function initMap() {
+	var location = {lat: 52.629781, lng: -1.139402};
+	var map = new google.maps.Map(document.getElementById("map"),{
+		zoom: 4,
+		center: location
+	});
+}
+</script>
+
+<script 
+async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBAQ5gSNY47KEgpyQIh312o2GkCwpPWmoM&callback=initMap">
+</script>
+
+
